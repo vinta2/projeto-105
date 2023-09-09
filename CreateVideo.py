e4de9818ba7df3c3cf979b763f33703303d8043d
vinta2 <102837440+vinta2@users.noreply.github.com>
2023-09-09 19:11:43 -0300

    Add files via upload

---
+++ CreateVideo.py
@@ -0,0 +1,11 @@
+import os
+import cv2
+
+path="images/"
+
+if ext in ['.gif','.png','.jpg','.jpeg','.jfif']:
+    file_name=path"/"file
+
+size=(width,height)
+
+out=cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'divx'),0.8,size)
